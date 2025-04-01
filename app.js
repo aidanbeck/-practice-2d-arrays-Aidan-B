c30f1590feb8c676f9b249175f6f8896747d650d
aidanbeck <aidanmbeck@outlook.com>
2025-03-31 22:51:07 -0500

    complete assignment

---
+++ app.js
@@ -0,0 +1,16 @@
+let allScores = [
+    [99, 75, 84],
+    [100, 137, 14],
+    [80, 81, 82]
+]
+
+let theaterSeating = [
+    ["A1", "A2", "A3", "A4"],
+    ["B1", "B2", "B3", "B4"],
+    ["C1", "C2", "C3", "C4"]
+];
+
+let contactList = [
+    ["Natalie", "nkies@fakemail.net", "555-555-5555"],
+    ["Nick", "rebelwithaclaus1337@aol.com", "877-446-6723"]
+]
